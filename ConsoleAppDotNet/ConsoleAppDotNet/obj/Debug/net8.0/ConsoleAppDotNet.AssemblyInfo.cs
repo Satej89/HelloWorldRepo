066354aed26a59c7206a4f9b304c86dcf32f054c
satej tupare <satejtupare@gmail.com>
2024-07-21 16:25:57 +0530

    the go project is added

--- ConsoleAppDotNet/ConsoleAppDotNet/obj/Debug/net8.0/ConsoleAppDotNet.AssemblyInfo.cs
+++ ConsoleAppDotNet/ConsoleAppDotNet/obj/Debug/net8.0/ConsoleAppDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de85a17f57bada3953567911d3e2e52649269da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
